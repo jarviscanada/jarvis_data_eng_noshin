a92d251fe76ad647c8c721ae9b1655c0dd15d541
nnsadat <noshin.nawar.sadat@gmail.com>
2020-03-04 05:49:06 +0000

    script to run Postgresql

---
+++ linux_sql/scripts/psql_docker.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#validate number of arguments
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+#store arguments in variables
+pw=$2
+
+#if docker is not running, start docker
+sudo systemctl status docker || sudo systemctl start docker
+
+#if `jrvs-psql` container is running, exit
+if [ $(sudo docker ps -f name=jrvs-psql | wc -l) = 2 ]; then
+	exit 0
+fi
+
+#if `pgdata` volume is not created, create `pgdata` volume
+if [ $(sudo docker volume ls -q -f name='pgdata' | wc -l) = 0 ]; then
+        sudo docker volume create pgdata
+fi
+
+#if `jrvs-psql` container is not created, run a `jrvs-psql` container
+if [ $(sudo docker container ls -a -f name=jrvs-psql -q | wc -l) = 0 ]; then
+	sudo docker run --name jrvs-psql -e POSTGRES_PASSWORD=$pw -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+fi
+
+
+#start postgresql container
+sudo docker container start jrvs-psql
+
+exit 0
