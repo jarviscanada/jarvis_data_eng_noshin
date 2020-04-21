be3c00720ae9f8a6b119aea278befd984f18e8d9
nnsadat <noshin.nawar.sadat@gmail.com>
2020-04-21 06:30:10 +0000

    Implemented Java Data Structures

---
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/list/Employee.java
@@ -0,0 +1,115 @@
+package ca.jrvs.practice.dataStructure.list;
+
+public class Employee implements Comparable<Employee>{
+
+  private int id;
+  private String name;
+  private int age;
+  private long salary;
+
+  public Employee() {
+  }
+
+  public Employee(int id, String name, int age, long salary) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public long getSalary() {
+    return salary;
+  }
+
+  public void setSalary(long salary) {
+    this.salary = salary;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", age=" + age +
+        ", salary=" + salary +
+        '}';
+  }
+
+  /**
+   * Compares this object with the specified object for order.  Returns a negative integer, zero, or
+   * a positive integer as this object is less than, equal to, or greater than the specified
+   * object.
+   *
+   * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+   * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This implies that
+   * <tt>x.compareTo(y)</tt> must throw an exception iff
+   * <tt>y.compareTo(x)</tt> throws an exception.)
+   *
+   * <p>The implementor must also ensure that the relation is transitive:
+   * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+   * <tt>x.compareTo(z)&gt;0</tt>.
+   *
+   * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+   * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for all <tt>z</tt>.
+   *
+   * <p>It is strongly recommended, but <i>not</i> strictly required that
+   * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+   * class that implements the <tt>Comparable</tt> interface and violates this condition should
+   * clearly indicate this fact.  The recommended language is "Note: this class has a natural
+   * ordering that is inconsistent with equals."
+   *
+   * <p>In the foregoing description, the notation
+   * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+   * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+   * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+   * <i>expression</i> is negative, zero or positive.
+   *
+   * @param o the object to be compared.
+   * @return a negative integer, zero, or a positive integer as this object is less than, equal to,
+   * or greater than the specified object.
+   * @throws NullPointerException if the specified object is null
+   * @throws ClassCastException   if the specified object's type prevents it from being compared to
+   *                              this object.
+   */
+  @Override
+  public int compareTo(Employee o) {
+    if(this.getAge()<o.getAge()){
+      return -1;
+    }
+    else if(this.getAge()>o.getAge()){
+      return 1;
+    }
+    else if(this.getSalary()<o.getSalary()){
+      return -1;
+    }
+    else if(this.getSalary()>o.getSalary()){
+      return 1;
+    }else{
+      return 0;
+    }
+  }
+}
