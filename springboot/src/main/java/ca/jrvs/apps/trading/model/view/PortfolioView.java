dd06dd1080604140317c3f1083c2ad5614246c27
nnsadat <noshin.nawar.sadat@gmail.com>
2020-05-13 20:39:08 +0000

    implemented dashboard controller

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/view/PortfolioView.java
@@ -0,0 +1,17 @@
+package ca.jrvs.apps.trading.model.view;
+
+import ca.jrvs.apps.trading.model.view.SecurityRow;
+import java.util.List;
+
+public class PortfolioView {
+
+  private List<SecurityRow> securityRows;
+
+  public List<SecurityRow> getSecurityRows() {
+    return securityRows;
+  }
+
+  public void setSecurityRows(List<SecurityRow> securityRows) {
+    this.securityRows = securityRows;
+  }
+}
